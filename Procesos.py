607b81b19952dbd568f21c6ae06cab0157dc1b50
JonatanYMV <jonatan.medina@correounivalle.edu.co>
2025-03-12 10:03:25 -0500

    ensayo

---
+++ Procesos.py
@@ -0,0 +1,11 @@
+
+
+
+print("Procesos Fiducoome")
+    #proceso para los cursos 
+        #entra a https://aulavirtual.coomeva.com.co/inicio
+        #ingresa con en la parte de "Beneficiario o no asociados"
+        #ingresa con tu CC para el USER y PASSWORD
+        #NoTA: INGRESA SI ES POSIBLE CADA DIA PARA NO DEJAR ACUMULAR LOS CURSOS
+    
+    #
